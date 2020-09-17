51c022a01f8bdeae5a098d63fd8fe15f95b164e1
JH <jak_hac@uni-bremen.de>
2020-09-17 14:15:06 +0200

    Deleted sln files

--- chess_ai/Main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Board.h"
-#include "MoveGenerator.h"
-#include "Perft.h"
-
-int main();
--- chess_ai/Validator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Definition.h"
-
-int sideValid(const int side);
-int fileRankValid(const int fr);
-int pieceValidEmpty(const int pce);
-int pieceValid(const int pce);
-
